db9f26262613c4f2be1b2ece978f0c76e4eaf4b3
Zelda110 <Zelda_110@icloud.com>
2025-09-27 19:10:33 +0800

    Initial Commit

---
+++ CeruleanPlayer/CeruleanPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CeruleanPlayerApp.swift
+//  CeruleanPlayer
+//
+//  Created by ハイラル・ゼルダ on 27/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct CeruleanPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
